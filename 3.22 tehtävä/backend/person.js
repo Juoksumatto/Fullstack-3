251af13e03a18cda51ccededd5b4975e4b1a5d66
onni <holttaonni@gmail.com>
2026-03-13 10:20:29 +0200

    Add files via upload

---
+++ 3.22 tehtävä/backend/person.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 3,
+    required: true },
+  number: {
+    type: String,
+    minlength: 8,
+    required: true,
+    match: [/^\d{2,3}-\d+$/, 'Number must be in format XX-XXXXXXX or XXX-XXXXXXX']
+  }
+})
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Person', personSchema)
